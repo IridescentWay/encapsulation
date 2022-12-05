f29026ac699239df0873026075f464d349684042
VereLee <verelee@foxmail.com>
2022-12-05 11:12:34 +0800

    2022-12-5 ljh: first runnable part of encapsulation

---
+++ face_pthread.c
@@ -0,0 +1,312 @@
+﻿#include <ucos_ii.h>
+#include <face_pthread.h>
+#include <face_errno.h>
+#include <face_pthread_time.h>
+pthread_mutex_t g_pthread_lock = PTHREAD_MUTEX_INITIALIZER;
+
+pthread_t pthread_self(void) {
+	pthread_tcb_t *ptcb = NULL;
+	OS_ENTER_CRITICAL();
+	ptcb = OSTCBCur->OSTCBExtPtr;
+	OS_EXIT_CRITICAL();
+	if (ptcb == NULL) {
+		/* Create ptcb for native task in case that call pthread_self */
+		ptcb = (pthread_tcb_t *)malloc(sizeof(pthread_tcb_t));
+		if (ptcb == NULL) {
+			return NULL;
+		}
+		memset(ptcb, 0, sizeof(pthread_tcb_t));
+		ptcb->magic = PTHREAD_TCB_MAGIC;
+		ptcb->task = OSTCBCur;
+		OS_ENTER_CRITICAL();
+		OSTCBCur->OSTCBEventPtr = ptcb;
+		OS_EXIT_CRITICAL();
+	}
+	return (pthread_t) ptcb;
+}
+
+pthread_tcb_t* __pthread_get_tcb(pthread_t thread) {
+
+	pthread_tcb_t* ptcb = (pthread_tcb_t*) thread;
+	printf("ptcb = %d\n", ptcb->magic);
+	if ((ptcb == NULL) || (ptcb->magic != PTHREAD_TCB_MAGIC)) {
+		return NULL;
+	}
+
+	return ptcb;
+}
+
+int pthread_create(pthread_t *thread,
+       const pthread_attr_t * attr,
+       void *(*start_routine)(void*), void * arg){
+	int ret = 0;
+
+	pthread_tcb_t *ptcb = NULL;
+	pthread_tcb_t *ptcb_c = NULL;
+
+	if ((thread == NULL) || ((attr != NULL) && (attr->flag != PTHREAD_DYN_INIT))
+		|| (start_routine == NULL)) {
+		return EINVAL;
+	}
+
+	*thread = NULL;
+
+	ptcb = (pthread_tcb_t *)malloc(sizeof(pthread_tcb_t));
+	if (ptcb == NULL) {
+		return ENOMEM;
+	}
+
+	memset(ptcb, 0, sizeof(pthread_tcb_t));
+	ptcb->magic = PTHREAD_TCB_MAGIC;
+
+	if (attr != NULL) {
+		ptcb->attr = *attr;
+		if (attr->inheritsched == PTHREAD_INHERIT_SCHED) {
+			ptcb_c = __pthread_get_tcb(pthread_self());
+			if (ptcb_c != NULL) {
+				ptcb->attr = ptcb_c->attr;
+			}
+		}
+	}
+	else {
+		ret = pthread_attr_init(&(ptcb->attr));
+		if (ret != 0) {
+			if (ptcb != NULL) {
+				ptcb->magic = 0;
+				free(ptcb);
+			}
+			return ret;
+		}
+	}
+
+	if (ptcb->attr.detachstate == PTHREAD_CREATE_JOINABLE) {
+		ret = sem_init(&(ptcb->join_sem), 0, 1);    // ljh: 将0改成1防止报错，为什么这里要value = 0？
+		if (ret != 0) {
+			ret = -1;
+			if (ptcb != NULL) {
+				ptcb->magic = 0;
+				free(ptcb);
+			}
+			return ret;
+		}
+	}
+
+	ptcb->thread_entry = start_routine;
+	ptcb->thread_para = arg;
+
+	strncpy(ptcb->thread_name, "posix_thread", PTHREAD_NAME_MAX_LEN);
+
+	if (ptcb->attr.schedparam.sched_priority < 0) {
+		ret = -1;
+		sem_destroy(&(ptcb->join_sem));
+	}
+
+	OS_ENTER_CRITICAL();
+	ret = OSTaskCreateExt((void(*)(void *))start_routine,              /* Create the start task                                */
+		(void          *)0,
+		(OS_STK        *)((int *)ptcb->attr.stackaddr + sizeof(CPU_STK)*(ptcb->attr.stacksize - 1)),
+		(INT8U)ptcb->attr.schedparam.sched_priority,
+		(INT16U)*thread,
+		(OS_STK        *)ptcb->attr.stackaddr,
+		(INT32U)ptcb->attr.stacksize,
+		(void          *)ptcb,
+		(INT16U)(OS_TASK_OPT_STK_CHK | OS_TASK_OPT_STK_CLR));
+
+	if (ret != 0) {
+		ret = -1;
+		sem_destroy(&(ptcb->join_sem));
+	}
+	ptcb->task = OSTCBPrioTbl[ptcb->attr.schedparam.sched_priority];
+	OS_TCB *ptask_cp = NULL;
+	ptask_cp = ptcb->task;
+	ptask_cp->OSTCBExtPtr = ptcb;
+	OS_EXIT_CRITICAL();
+
+	*thread = ptcb;
+	pthread_tcb_t * _ptcb = (pthread_tcb_t *)(*thread);
+	return 0;
+}
+
+int pthread_detach(pthread_t thread)
+{
+    int ret = 0;
+    pthread_tcb_t *ptcb = NULL;
+
+    if (thread == NULL) {
+        return EINVAL;
+    }
+
+    ptcb = __pthread_get_tcb(thread);
+    if (ptcb == NULL) {
+        return EINVAL;
+    }
+
+    if (ptcb->attr.detachstate == PTHREAD_CREATE_DETACHED) {
+        return EINVAL;
+    } else {
+        ret = pthread_mutex_lock(&g_pthread_lock);
+        if (ret != 0) {
+            return EAGAIN;
+        }
+        ptcb->attr.detachstate = PTHREAD_CREATE_DETACHED;
+        pthread_mutex_unlock(&g_pthread_lock);
+    }
+
+    return 0;
+}
+void pthread_cleanup_pop(int execute)
+{
+    pthread_tcb_t *ptcb = NULL;
+    pthread_cleanup_t *cleanup = NULL;
+
+    ptcb = __pthread_get_tcb(pthread_self());
+    if (ptcb == NULL) {
+        return;
+    }
+
+    cleanup = ptcb->cleanup;
+    if (cleanup != NULL) {
+        ptcb->cleanup = cleanup->prev;
+        if (execute != 0) {
+            cleanup->cleanup_routine(cleanup->para);
+        }
+        free(cleanup);
+    }
+}
+void pthread_cleanup_push(void (*routine)(void *), void *arg)
+{
+    pthread_tcb_t *ptcb = NULL;
+    pthread_cleanup_t *cleanup = NULL;
+
+    ptcb = __pthread_get_tcb(pthread_self());
+    if (ptcb == NULL) {
+        return;
+    }
+
+    cleanup = (pthread_cleanup_t *) malloc(sizeof(pthread_cleanup_t));
+    if (cleanup != NULL) {
+        cleanup->cleanup_routine = routine;
+        cleanup->para = arg;
+        cleanup->prev = ptcb->cleanup;
+        ptcb->cleanup = cleanup;
+    }
+}
+int pthread_once(pthread_once_t *once_control, void (*init_routine)(void))
+{
+    int ret = 0;
+
+    ret = pthread_mutex_lock(&g_pthread_lock);
+    if (ret != 0) {
+        return EAGAIN;
+    }
+
+    if (*once_control == PTHREAD_ONCE_INIT)
+    {
+        *once_control = !PTHREAD_ONCE_INIT;
+
+        pthread_mutex_unlock(&g_pthread_lock);
+
+        init_routine ();
+        return 0;
+    }
+
+    pthread_mutex_unlock(&g_pthread_lock);
+
+    return 0;
+}
+int pthread_getcpuclockid(pthread_t thread, clockid_t *clock_id)
+{
+    if ((thread == NULL) || (clock_id == NULL)) {
+        return EINVAL;
+    }
+
+    *clock_id = CLOCK_MONOTONIC;
+
+    return 0;
+}
+
+int pthread_setname_np(pthread_t thread, const char *name)
+{
+    pthread_tcb_t *ptcb = NULL;
+
+    if ((thread == NULL) || (name == NULL)) {
+        return EINVAL;
+    }
+
+    ptcb = __pthread_get_tcb(thread);
+    if (ptcb == NULL) {
+        return EINVAL;
+    }
+
+    /* Truncate the name if it's too long. */
+    OS_MemCopy(ptcb->thread_name, name, PTHREAD_NAME_MAX_LEN);
+    if (OS_StrLen(name) > PTHREAD_NAME_MAX_LEN) {
+        return ERANGE;
+    }
+
+    return 0;
+}
+
+int pthread_getname_np(pthread_t thread, char *name, size_t len)
+{
+    pthread_tcb_t *ptcb = NULL;
+
+    if ((thread == NULL) || (name == NULL)) {
+        return EINVAL;
+    }
+
+    if (len < PTHREAD_NAME_MAX_LEN) {
+        return ERANGE;
+    }
+
+    ptcb = __pthread_get_tcb(thread);
+    if (ptcb == NULL) {
+        return EINVAL;
+    }
+
+    memset(name, 0, len);
+    OS_MemCopy(name, ptcb->thread_name, len);
+
+    return 0;
+}
+
+int pthread_getconcurrency(void)
+{
+    
+    return 0;
+}
+
+int pthread_setconcurrency(int new_level)
+{
+    
+    return ENOSYS;
+}
+int pthread_equal(pthread_t t1, pthread_t t2)
+{
+    return (int)(t1 == t2);
+}
+int pthread_cancel(pthread_t thread)
+{
+    return ENOSYS;
+}
+
+void pthread_testcancel(void)
+{
+    return;
+}
+
+int pthread_setcancelstate(int state, int *oldstate)
+{
+    return ENOSYS;
+}
+
+int pthread_setcanceltype(int type, int *oldtype)
+{
+    return ENOSYS;
+}
+
+int pthread_kill(pthread_t thread, int sig)
+{
+    return ENOSYS;
+}
+
---
+++ face_pthread.h
@@ -0,0 +1,189 @@
+#include "ucos_ii.h"
+#include "face_time.h"
+#include "face_sched.h"
+
+#ifndef   FACE_PTHREAD_H
+#define   FACE_PTHREAD_H
+
+// ↓↓↓↓↓↓↓↓↓↓ 曾经张继露定义在ucosii.h中的
+#define SCHED_OTHER 0
+#define SCHED_FIFO  1
+#define SCHED_RR    2
+#define SCHED_CFS   3
+
+#define PTHREAD_DEFAULT_SLICE          10u
+#define PTHREAD_CREATE_JOINABLE        0U
+#define PTHREAD_CREATE_DETACHED        1U
+#define PTHREAD_DEFAULT_STACK_SIZE     2048U
+#define PTHREAD_DEFAULT_GUARD_SIZE     256U
+#define PTHREAD_DEFAULT_PRIORITY       30U
+#define PTHREAD_NAME_MAX_LEN           16U
+
+#define pthread_flase                  0u
+
+#define PTHREAD_MUTEX_NORMAL 0
+#define PTHREAD_MUTEX_DEFAULT 1
+#define PTHREAD_MUTEX_RECURSIVE 1
+#define PTHREAD_MUTEX_ERRORCHECK 2
+
+#define PTHREAD_PRIO_NONE    0
+#define PTHREAD_PRIO_INHERIT 1
+#define PTHREAD_PRIO_PROTECT 2
+
+#define PTHREAD_INHERIT_SCHED 0
+#define PTHREAD_EXPLICIT_SCHED 1
+
+#define PTHREAD_SCOPE_SYSTEM 0
+#define PTHREAD_SCOPE_PROCESS 1
+
+#define PTHREAD_PROCESS_PRIVATE 0
+#define PTHREAD_PROCESS_SHARED 1
+
+#define PTHREAD_CANCEL_ENABLE 0
+#define PTHREAD_CANCEL_DISABLE 1
+#define PTHREAD_CANCEL_MASKED 2
+#define PTHREAD_CANCEL_DEFERRED 0
+#define PTHREAD_CANCEL_ASYNCHRONOUS 1
+#define PTHREAD_CANCELED ((void *)-1)
+
+#define PTHREAD_DYN_INIT    0X01
+#define PTHREAD_STATIC_INIT 0X02
+#define PTHREAD_MUTEX_INITIALIZER {PTHREAD_STATIC_INIT, NULL, {0}}
+#define PTHREAD_COND_INITIALIZER  {PTHREAD_STATIC_INIT, 0, 0, NULL, NULL, NULL, {0}}
+#define PTHREAD_ONCE_INIT 0
+
+#define NULL ((void *)0)
+// ↑↑↑↑↑↑↑↑↑↑↑↑↑ 曾经张继露定义在ucosii.h中的
+
+#define PTHREAD_DEFAULT_STACK_SIZE 2048
+#define PTHREAD_DEFAULT_GUARD_SIZE 256
+#define PTHREAD_DEFAULT_PRIORITY   30
+#define PTHREAD_DEFAULT_SLICE      10
+#define PTHREAD_NAME_MAX_LEN  16
+
+#define PTHREAD_TCB_MAGIC 0X11223344
+
+#define DEFAULT_MUTEX_TYPE        PTHREAD_MUTEX_DEFAULT
+#define DEFAULT_MUTEX_PROCOCOL    PTHREAD_PRIO_INHERIT
+#define DEFAULT_MUTEX_PRIOCEILING 30
+#define DEFAULT_MUTEX_PSHARED     PTHREAD_PROCESS_PRIVATE
+
+#define DEFAULT_COND_CLOCK  CLOCK_REALTIME
+#define DEFAULT_COND_SHARED PTHREAD_PROCESS_PRIVATE
+/* Defined for API with delay time */
+#define uos_WAIT_FOREVER 0xffffffffu /**< 阻塞性等待，即一直等待，直到事件发生或资源获得才返回 */
+#define uos_NO_WAIT      0x0         /**< 非阻塞性等待，即若无事件发生或无资源可获得，则返回 */
+
+typedef void * uos_hdl_t;
+typedef uos_hdl_t uos_mutex_t;
+typedef INT32U size_t;
+typedef INT32U pthread_t;
+typedef int pthread_once_t;
+//typedef struct os_event pthread_mutex_t;
+//typedef unsigned int pthread_mutexattr_t;
+typedef unsigned char uint8_t;
+
+typedef struct pthread_attr_t
+{
+	uint8_t flag;
+    int        detachstate;         //线程的分离状态
+    int        schedpolicy;         //线程调度策略
+	struct sched_param schedparam;  //线程调度参数
+    int         inheritsched;        //线程的继承性
+    int         scope;         //线程的作用域
+    long unsigned int      guardsize;         //线程栈末尾的警戒缓冲区大小
+    int         stackaddr_set;
+    void *      stackaddr;         //线程栈的位置
+	long unsigned int      stacksize;         //线程栈的大小
+} pthread_attr_t;
+
+typedef struct pthread_mutexattr {
+    uint8_t flag;
+    int type;
+    int protocol;
+    int prioceiling;
+    int pshared;
+    INT8U   prio;
+    INT8U  *perr;
+} pthread_mutexattr_t;
+
+typedef struct pthread_mutex {
+    uint8_t              flag;
+    OS_EVENT                *mutex;
+    pthread_mutexattr_t  attr;
+} pthread_mutex_t;
+
+typedef struct pthread_cleanup {
+    int cancel_type;
+    struct pthread_cleanup *prev;
+    void (*cleanup_routine)(void *para);
+    void *para;
+} pthread_cleanup_t;
+
+typedef struct pthread_tcb {
+    void *task;    /* The uc task handle. */
+    unsigned int magic; /* The pthread tcb memory magic number. */
+    void *(*thread_entry)(void *para); /* The start routine of the thread. */
+    void *thread_para;   /* The parameter of start routine. */
+    void *join_sem;  /* The semaphore for pthread_join. */
+    pthread_cleanup_t *cleanup; /* The registered cleanup function for the thread.*/
+    void *environ;
+    void **tls;
+    void  *return_value; /* The thread's return value. */
+    pthread_attr_t  attr; /* The thread's attribute. */
+    char thread_name[PTHREAD_NAME_MAX_LEN + 1];  /* The thread's name. */
+} pthread_tcb_t;
+
+pthread_tcb_t* __pthread_get_tcb(pthread_t thread);
+
+int       pthread_create(pthread_t *thread, const pthread_attr_t *attr,
+                         void *(*start_routine)(void *), void *arg);
+int       pthread_detach(pthread_t thread);
+int       pthread_cancel(pthread_t thread);
+void      pthread_testcancel(void);
+
+pthread_t pthread_self(void); // need to support
+
+int       pthread_setcancelstate(int state, int *oldstate);
+int       pthread_setcanceltype(int type, int *oldtype);
+int       pthread_kill(pthread_t thread, int sig);
+int       pthread_equal(pthread_t t1, pthread_t t2);
+void      pthread_cleanup_pop(int execute);
+void      pthread_cleanup_push(void (*routine)(void *), void *arg);
+int       pthread_once(pthread_once_t *once_control, void (*init_routine)(void));
+int       pthread_getcpuclockid(pthread_t thread_id, clockid_t *clock_id);
+int       pthread_setconcurrency(int new_level);
+int       pthread_getconcurrency(void);
+int       pthread_setname_np(pthread_t thread, const char *name);
+int       pthread_getname_np(pthread_t thread, char *name, size_t len);
+
+int pthread_attr_init(pthread_attr_t *attr);
+int pthread_attr_destroy(pthread_attr_t *attr);
+int pthread_attr_setdetachstate(pthread_attr_t *attr, int detachstate);
+int pthread_attr_getdetachstate(const pthread_attr_t *attr, int *detachstate);
+int pthread_attr_setschedpolicy(pthread_attr_t *attr, int policy);
+int pthread_attr_getschedpolicy(pthread_attr_t const *attr, int *policy);
+int pthread_attr_setschedparam(pthread_attr_t *attr, const struct sched_param *param);
+int pthread_attr_getschedparam(const pthread_attr_t *attr, struct sched_param *param);
+int pthread_attr_setstacksize(pthread_attr_t *attr, size_t stacksize);
+int pthread_attr_getstacksize(const pthread_attr_t *attr, size_t *stacksize);
+int pthread_attr_setstackaddr(pthread_attr_t *attr, void *stackaddr);
+int pthread_attr_getstackaddr(const pthread_attr_t *attr, void **stackaddr);
+int pthread_attr_setstack(pthread_attr_t *attr, void *stackaddr, size_t stacksize);
+int pthread_attr_getstack(const pthread_attr_t *attr, void **stackaddr, size_t *stacksize);
+int pthread_attr_setinheritsched(pthread_attr_t *attr, int inheritsched);
+int pthread_attr_getinheritsched(const pthread_attr_t * attr, int * inheritsched);
+int pthread_attr_setguardsize(pthread_attr_t *attr, size_t guardsize);
+int pthread_attr_getguardsize(const pthread_attr_t *attr, size_t *guardsize);
+int pthread_attr_setscope(pthread_attr_t *attr, int contentionscope);
+int pthread_attr_getscope(const pthread_attr_t *attr, int *contentionscope);
+
+int pthread_mutex_init(pthread_mutex_t *mutex, const pthread_mutexattr_t *attr);
+int pthread_mutex_destroy(pthread_mutex_t *mutex);
+int pthread_mutex_lock(pthread_mutex_t *mutex);
+int pthread_mutex_unlock(pthread_mutex_t *mutex);
+int pthread_mutex_trylock(pthread_mutex_t *mutex);
+int pthread_mutex_timedlock(pthread_mutex_t *mutex, const struct timespec *at);
+int pthread_mutex_getprioceiling(const pthread_mutex_t *mutex, int *prioceiling);
+
+#endif
